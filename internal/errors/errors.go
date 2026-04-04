1ae27f1dd4b924206216609bc5154f398a354163
Eduard <allen-2002@mail.ru>
2026-04-05 00:01:49 +0300

    gofmt

--- internal/errors/errors.go
+++ internal/errors/errors.go
@@ -3,6 +3,6 @@ package myerrors
 import "errors"
 
 var (
-	ErrNoRows error = errors.New("no rows")
+	ErrNoRows     error = errors.New("no rows")
 	ErrUserExists error = errors.New("user already exists")
 )
